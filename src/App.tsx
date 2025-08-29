b65f720133d2b66ac87dc15071293c313b351da6
Aditya Vardhan Singh <aditya.vardhan.singh@outlook.com>
2025-08-29 23:52:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='h-screen flex flex-row justify-center items-center bg-indigo-600'>
+      <h1 className='text-3xl font-bold text-white'>Welcome to your application's home page.</h1>
+    </div>
+  )
+}
+
+export default App
